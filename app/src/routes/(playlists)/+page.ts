944d2c6daeda78a0f1953c9d706244ee42d2d581
MBeliou <maxime.bele@gmail.com>
2025-05-14 11:56:06 +0200

    parallel layout cascade

--- app/src/routes/(playlists)/+page.ts
+++ app/src/routes/(playlists)/+page.ts
@@ -3,16 +3,23 @@ import type { PageLoad } from './$types';
 export const load = (async ({ parent }) => {
 	const { supabase } = await parent();
 
-	const { data: recommendedArtists } = await supabase.from('artists').select('*').limit(10);
-	const { data: recommendedTracks } = await supabase
-		.from('tracks')
-		.select('*, artist: artists(artist_name), album: albums(cover_url)')
-		.limit(10);
-	const { data: recommendedAlbums } = await supabase.from('albums').select('*').limit(10);
-	const { data: recommendedPlaylists } = await supabase
-		.from('playlists')
-		.select('*, tracks(*), creator:users!playlists_user_id_fkey(*)')
-		.limit(10);
+	const [
+		{ data: recommendedArtists },
+		{ data: recommendedTracks },
+		{ data: recommendedAlbums },
+		{ data: recommendedPlaylists }
+	] = await Promise.all([
+		supabase.from('artists').select('*').limit(10),
+		supabase
+			.from('tracks')
+			.select('*, artist: artists(artist_name), album: albums(cover_url)')
+			.limit(10),
+		supabase.from('albums').select('*').limit(10),
+		supabase
+			.from('playlists')
+			.select('*, tracks(*), creator:users!playlists_user_id_fkey(*)')
+			.limit(10)
+	]);
 
 	return {
 		recommendations: {
